27c025c4a1f6937da30a1e4e42eacc6730da739f
ddobi <qhdqhdekd261@naver.com>
2022-02-28 00:46:32 +0900

    end of streams section

--- components/layout.tsx
+++ components/layout.tsx
@@ -119,11 +119,11 @@ export default function Layout({ title, canGoBack, hasTabBar, children }: Layout
               <span>채팅</span>
             </a>
           </Link>
-          <Link href="/live">
+          <Link href="/streams">
             <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
-                router.pathname === "/live"
+                router.pathname === "/streams"
                   ? "text-orange-500"
                   : "hover:text-gray-500 transition-colors"
               )}
---
+++ pages/api/streams/index.ts
@@ -0,0 +1,47 @@
+import { Stream } from "@prisma/client";
+import { withIronSessionApiRoute } from "iron-session/next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    session: { user },
+    body: { name, price, description },
+  } = req;
+  if (req.method === "POST") {
+    const stream = await client.stream.create({
+      data: {
+        name,
+        price,
+        description,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      stream,
+    });
+  } else if (req.method === "GET") {
+    const streams = await client.stream.findMany({
+      take: 10,
+      skip: 20,
+    });
+    res.json({
+      ok: true,
+      streams,
+    });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+  })
+);
