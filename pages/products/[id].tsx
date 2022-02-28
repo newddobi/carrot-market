13219488856b214db5aa7d0bf6b5feca4d52ab56
ddobi <qhdqhdekd261@naver.com>
2022-02-28 14:45:05 +0900

    complete image upload

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -39,9 +39,15 @@ const ItemDetail: NextPage = () => {
     <Layout canGoBack>
       <div className="px-4  py-4">
         <div className="mb-8">
-          <div className="h-96 bg-slate-300" />
+          <img
+            src={`https://imagedelivery.net/_fA90_CnLcX38I7jOJ6-uw/${data?.product.image}/public`}
+            className="bg-slate-300 object-cover"
+          />
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <img
+              src={`https://imagedelivery.net/_fA90_CnLcX38I7jOJ6-uw/${data?.product.user.avatar}/avatar`}
+              className="w-12 h-12 rounded-full bg-slate-300"
+            />
             <div>
               <p className="text-sm font-medium text-gray-700">{data?.product?.user?.name}</p>
               <Link href={`/users/profiles/${data?.product?.user?.id}`}>
